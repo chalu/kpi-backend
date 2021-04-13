9dba9cfb5ee09d67ef68f74f4ce8ba37af206154
Charles Odili <chaluwa@gmail.com>
2021-04-13 10:08:00 +0100

    generate openapi models

---
+++ src/api-spec/index.ts
@@ -0,0 +1,6 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type { APICallsKPIRequest } from './models/APICallsKPIRequest';
+export type { APICallsKPIResponse } from './models/APICallsKPIResponse';
+export type { ErrorResponse } from './models/ErrorResponse';
