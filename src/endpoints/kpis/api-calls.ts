c76df9cda506be5f69bc6385e5ce47ac10584950
Charles Odili <chaluwa@gmail.com>
2021-04-14 17:03:24 +0100

    query DB for all sandbox API calls

--- src/endpoints/kpis/api-calls.ts
+++ src/endpoints/kpis/api-calls.ts
@@ -1,21 +1,25 @@
 import express, { Request } from "express";
+import { PrismaClient, ApiLog } from "@prisma/client";
+
 import { APICallsKPIResponse } from "../../oas-contract"
- 
+
 import { ApiHandler, respondAs, respondWith } from "../core";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
-const getSandboxAPICalls: ApiHandler<APICallsKPIResponse> = async (req: Request) => {
+const getSandboxAPICalls: ApiHandler<APICallsKPIResponse> = async (reqquest: Request) => {
   let response;
   try {
-
+    const allCalls: ReadonlyArray<ApiLog> = await prisma.apiLog.findMany();
     const payload: APICallsKPIResponse = {
-        calls:604
+        calls:allCalls.length
     };
 
     response = respondAs(200, payload);
 
   } catch (e) {
+    console.log(e.message);
     response = e;
   }
 
