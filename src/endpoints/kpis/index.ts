94eda2dc996fa7f1e241966adb9cc92fd2b59f79
Charles Odili <chaluwa@gmail.com>
2021-04-16 15:50:40 +0100

    get number of sandbox API downloads

--- src/endpoints/kpis/index.ts
+++ src/endpoints/kpis/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import APICallsRouter from './api-calls';
+import APIDownloadsRouter from './api-downloads';
 
 const router = express.Router();
 
 // Mount the endpoint controllers
 router.use('/sandbox-api-calls', APICallsRouter);
+router.use('/sandbox-api-downloads', APIDownloadsRouter);
 
 export default router;
